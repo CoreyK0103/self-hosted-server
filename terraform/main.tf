b8b875d73801b8a45fb4b508c2ec162aff9be193
Corey Knight <142681177+CoreyK0103@users.noreply.github.com>
2026-06-10 20:45:42 +0100

    ci: deployment pipelines (#1)

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+module "s3_bucket" {
+  #checkov:skip=CKV_AWS_144
+  #checkov:skip=CKV2_AWS_61
+  #checkov:skip=CKV_AWS_18
+  #checkov:skip=CKV2_AWS_62
+
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=v5.14.0"
+
+  bucket        = "self-hosted-server-init-bucket"
+  acl           = "private"
+  force_destroy = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
