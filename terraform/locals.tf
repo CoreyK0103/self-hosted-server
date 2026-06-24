08b1312f5546d3f154199ce5529d21b1090f81ad
CoreyK0103 <coreyjknight@outlook.com>
2026-06-24 21:29:00 +0100

    feat: vpc and nat gateway

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,4 +2,14 @@ locals {
   project_name    = "self-hosted-server"
   short_env       = var.environment == "production" ? "prod" : "dev"
   resource_prefix = "${local.project_name}-${local.short_env}"
+
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
+
+  tags = {
+    Name        = local.project_name
+    Environment = var.environment
+  }
 }
---
+++ terraform/vpc.tf
@@ -0,0 +1,33 @@
+module "vpc" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v6.6.1"
+
+  name = "${local.resource_prefix}-vpc"
+  cidr = local.vpc_cidr
+  azs  = local.azs
+
+  public_subnets  = local.public_subnets
+  private_subnets = local.private_subnets
+
+  create_igw           = true
+  enable_nat_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.tags
+}
+
+module "fck-nat" {
+  source = "git::https://github.com/RaJiska/terraform-aws-fck-nat.git?ref=v1.6.0"
+
+  name      = "${local.resource_prefix}-nat"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnets[0]
+  ha_mode   = true
+
+  update_route_tables = true
+  route_tables_ids    = module.vpc.private_route_table_ids
+
+  instance_type = "tg4.nano"
+
+  tags = local.tags
+}
