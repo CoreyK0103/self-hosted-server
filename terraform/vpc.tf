12b9a9307007350d73e095e1e2d1e043afa4191e
Corey Knight <142681177+CoreyK0103@users.noreply.github.com>
2026-06-29 20:08:07 +0100

    feat: vpc and nat gateway (#9)

---
+++ terraform/vpc.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=3ffbd46fb1c7733e1b34d8666893280454e27436"
+
+  name = "${local.resource_prefix}-vpc"
+  cidr = local.vpc_cidr
+  azs  = local.azs
+
+  public_subnets = local.public_subnets
+  intra_subnets  = local.private_subnets
+
+  create_igw           = true
+  enable_nat_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.tags
+}
+
+module "fck-nat" {
+  source = "git::https://github.com/RaJiska/terraform-aws-fck-nat.git?ref=2ef15ba78c33ad2289afefdc233847b457548fbb"
+
+  name      = "${local.resource_prefix}-nat"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnets[0]
+  ha_mode   = true
+
+  update_route_tables = true
+  route_tables_ids = {
+    for idx, rt_id in module.vpc.intra_route_table_ids :
+    "private-route-table-${idx}" => rt_id
+  }
+
+  instance_type = "tg4.nano"
+
+  tags = local.tags
+}
