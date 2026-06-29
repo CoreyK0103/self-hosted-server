307e60a43ab5df964f25f26e46a8853f56694592
CoreyK0103 <coreyjknight@outlook.com>
2026-06-29 19:59:11 +0100

    chore: linting

--- lambda/test_lambda.py
+++ lambda/test_lambda.py
@@ -1,12 +1,17 @@
+"""Test lambda"""
+
 import json
 import os
 import urllib.request
 
+
 def handler(_event, _context):
+    """Lambda handler to fetch external IP address"""
     url = os.environ.get("IP_CHECK_URL", "https://api.ipify.org?format=json")
     try:
         with urllib.request.urlopen(url, timeout=10) as r:
             body = r.read().decode()
+    # pylint: disable=broad-except
     except Exception as e:
         print("error_fetching_ip:", e)
         return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
